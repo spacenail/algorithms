8360660d032aa364a824b99acec08d8387d49103
Artem Meshcherov <spacenail@gmail.com>
2022-04-15 00:01:21 +0300

    first task done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Exponentiation.exp(2,0));
+        System.out.println(Exponentiation.exp(2,1));
+        System.out.println(Exponentiation.exp(2,2));
+        System.out.println(Exponentiation.exp(2,3));
+        System.out.println(Exponentiation.exp(2,-1));
+        System.out.println(Exponentiation.exp(10,-2));
     }
 }
